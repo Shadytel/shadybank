bfb3778b63d68c3051207025476f9f1674103374
Karl Koscher <github@degdeg.com>
2022-07-07 18:14:33 -0700

    Update schema to add authorization status

--- srv/data/postgres.sql
+++ srv/data/postgres.sql
@@ -23,8 +23,11 @@ integer not null, amount numeric(8,2) not null, pan varchar(19), auth_code varch
 description text, constraint fk_debit_aid foreign key (debit_account) references accounts(id),
 constraint fk_credit_aid foreign key (credit_account) references accounts(id));
 
+create type auth_status as enum ('pending', 'posted', 'reversed', 'voided', 'expired');
+
 create table authorizations (id serial primary key, pan varchar(19) not null, auth_code varchar(6) not null, 
 debit_account int, credit_account int, authorized_debit_amount numeric(8,2), timestamp timestamp not null default NOW(), 
-expires timestamp default NOW() + INTERVAL '24 hours', constraint fk_debit_aid foreign key (debit_account) references accounts(id),
+expires timestamp default NOW() + INTERVAL '24 hours', status auth_status not null default 'pending',
+constraint fk_debit_aid foreign key (debit_account) references accounts(id),
 constraint fk_credit_aid foreign key (credit_account) references accounts(id));
 
