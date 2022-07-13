988e98a258ee3dee7e971cdc514ed83f9891189a
Karl Koscher <github@degdeg.com>
2022-07-13 14:57:22 -0700

    Activation fix

--- src/apiserver.py
+++ src/apiserver.py
@@ -422,7 +422,7 @@ class ShadyBucksAPIDaemon:
         args = await request.post()
         if not 'name' in args:
             raise web.HTTPBadRequest()
-        name = args['name'].upper()
+        name = str(args['name'].upper())
         merchant_data = await self._get_account_data(await self._get_auth_account(request))
         if not merchant_data['admin']:
             raise web.HTTPForbidden()
@@ -435,7 +435,7 @@ class ShadyBucksAPIDaemon:
             card_data['card']['pan'], name, str(dd1), dd2)
         return web.json_response({
             'track1': 'B' + str(card_data['card']['pan']) + '^' + name + '^' + \
-            str(card_data['card']['exp']) + '101' + card_data['card']['dd1'] + dd1,
+            str(card_data['card']['exp']) + '101' + str(card_data['card']['dd1']) + dd1,
             'track2': str(card_data['card']['pan']) + '=' + str(card_data['card']['exp']) + \
             '101' + dd2 })
 
