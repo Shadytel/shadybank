edb3703d2f69524b5689d551530744365d5bf7ca
Karl Koscher <github@degdeg.com>
2026-06-15 02:38:21 +0000

    Add support for 'pay me' URLs

--- src/frontend.py
+++ src/frontend.py
@@ -176,7 +176,7 @@ class ShadyBucksFrontEndDaemon:
         balance_resp = await self._api_client_session.get('http://api-endpoint:8080/api/balance', headers=auth_header)
         if balance_resp.status == 200:
             balance_json = await balance_resp.json()
-            context = { **context, **balance_json }
+            context = { **request.query, **context, **balance_json }
         else:
             raise web.HTTPFound('/app/login')
         return aiohttp_jinja2.render_template('transaction-entry.html', request, context)
